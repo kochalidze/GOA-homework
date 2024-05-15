7cb8b2c2110caa7c68528c981f831ff2f4c0ff40
kochalidze <kochalidzegiga11@gmail.com>
2024-05-15 14:39:34 +0400

    second commit

---
+++ Day 3/lesson 3.py
@@ -0,0 +1,12 @@
+#Let's use the print function: assembly
+print(5  +  10)
+
+
+#Let's use the print function: deduction
+print(5  -  10)
+
+#Let's use the print function: multiplication
+print(5  *  10)
+
+#Let's use the print function: division
+print(5  /  10)
