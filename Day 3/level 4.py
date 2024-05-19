4acdfae7d4ba0e6aa8d1d427cba260d1c0f4ba6d
kochalidze <kochalidzegiga11@gmail.com>
2024-05-19 17:44:21 +0400

    second commit

---
+++ Day 3/level 4.py
@@ -0,0 +1,37 @@
+#thing and price of thing
+price = 20
+book = 2
+x = price / book
+print(price / book)
+
+
+#me age and age of family member
+age1 = 13
+age2 = 38
+print(age1 + 10)
+print(age2 + 10)
+
+#The result of arithmetic operations
+x = 12
+y = 6
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+#me name and age of me
+name = "Giga"
+age = 13
+print(str(name))
+print(int(age))
+
+
+num1 = (str("please enter firsr numbe"))
+num2 = (str("please enter second number"))
+
+print(5 + 5)
+
+num1 = 15
+
+print(num1 + 10)
+
