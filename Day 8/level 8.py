8656ab33bb7d9a5e13b05c99d0d873d46b73b95f
kochalidze <kochalidzegiga11@gmail.com>
2024-05-28 16:01:03 +0400

    second commit

---
+++ Day 8/level 8.py
@@ -0,0 +1,88 @@
+#5-5 მაგალითი ოპერატორებიდან (>, <, ==, >=, <=, !=)
+
+
+#პირველი ოპერაცია (>)
+#true
+print(9 > 8)
+print(5 > 3)
+print(11 > 10)
+#false
+print(44 > 55)
+print(18 > 35)
+
+#მეორე ოპერსცის (<)
+#true
+print(11 < 15)
+print(7 < 8)
+print(43 < 49)
+#false
+print(88 < 79)
+print(12 < 11)
+
+#მესამე ოპერსცის (==)
+#true
+print(13 == 13)
+print(2 == 2)
+print(41 == 41)
+#false
+print(77 == 79)
+print(5 == 6)
+
+#მეოთხე ოპერსცის (>=)
+#true
+print(50 >= 46)
+print(33 >= 33)
+print(40 >= 35)
+#false
+print(20 >= 30)
+print(22 >= 90)
+
+
+#მეხუთე ოპერსცის (<=)
+#true
+print(88 <= 100)
+print(44 <= 44)
+print(40 <= 35)
+#false
+print(66 <= 54)
+print(64 <= 38)
+
+
+#მეექვსე ოპერსცის (!=)
+#true
+print(90 != 10)
+print(86 != 43)
+print(57 != 35)
+#false
+print(55 != 55)
+print(22 != 22)
+
+#10-10 მაგალითი and და or ოპერატორებზე
+
+#პირველი ოპერაცია (and)
+#True
+print(True and True)
+print(False and False)
+print(6>3 and False)
+print(5<3 and True)
+print(True and False)
+print(True and False)
+print(True and False)
+print(True and False)
+print(True and False)
+print(True and False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
