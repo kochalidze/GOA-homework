a43b9224995d1a43e2e47a2098def6ac832f93cd
kochalidze <kochalidzegiga11@gmail.com>
2024-05-29 23:27:37 +0400

    second commit

--- level 2/homework/me new friends.py
+++
@@ -1,5 +0,0 @@
-name1 = "Gio shavadze"
-name2 = "vazha mgeladze"
-name3 = "sandro miqeladze"
-
-print("I " + "made " + "friends " + "in " + "GOa: " + name1 + name2 + name3 + "and " + "even " + "others ")
