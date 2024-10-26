e45da5ee4a714bcd0ee4b2bf4988e40d31f4da91
kochalidze <kochalidzegiga11@gmail.com>
2024-10-26 22:01:20 +0400

    second commit

--- level 73/classwork/script.js
+++
@@ -1,5 +0,0 @@
-// 1) html-ში შექმენით ორი პარაგრაფი, შემდეგ  js-ში აირჩიეთ ეს ორი პარაგრაფი და მიეცით ფერი, არ გამოიყენოთ მთოდები როგორიცაა .getElementById() და ა.შ.
-
-const  paragraphs = document.getElementById('p1');
-const  paragraphs2 = document.getElementById('p2');
-
